cd3bbcc93e7f0be923848b27aa82cb3d3dc53573
Steper2000 <okunev-85@bk.ru>
2019-06-15 15:25:49 +0300

    function sale() ready

--- java.js
+++ java.js
@@ -1,6 +1,10 @@
 function sale(){
     var priceM=+document.querySelector('#price-m').textContent;
-    document.querySelector("#price-6m-m").textContent=priceM;
-    document.querySelector("#price-1y-m").textContent=priceM*0.85;
-    document.querySelector("#price-3y-m").textContent=priceM*0.7;
+    document.querySelector("#price-6m-m").textContent='или '+priceM*0.9 +" руб";
+    document.querySelector("#price-1y-m").textContent='или '+priceM*0.85 +" руб";
+    document.querySelector("#price-3y-m").textContent='или '+priceM*0.7 +" руб";
+}
+
+function select(){
+    var tarif =document.querySelector('#trafic')
 }
