2979d37b22ffea2bb3e21b6c7e8b9aaf208dd420
Steper2000 <okunev-85@bk.ru>
2019-06-15 14:54:10 +0300

    JS code connected with hosting window

--- java.js
+++ java.js
@@ -1,4 +1,6 @@
-function sale{
-    var priceM=+document.querySelector('#price-m').value;
-    document.querySelector("#price-6m-m").textContent(priceM)
+function sale(){
+    var priceM=+document.querySelector('#price-m').textContent;
+    document.querySelector("#price-6m-m").textContent=priceM;
+    document.querySelector("#price-1y-m").textContent=priceM*0.85;
+    document.querySelector("#price-3y-m").textContent=priceM*0.7;
 }
